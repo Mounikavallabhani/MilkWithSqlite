5c57b7e71441204ac35bf56df6172e4551969a18
NARASIMHA <narasimha.1995>
2018-11-20 23:24:25 -0800

    n commit

--- app/src/main/java/com/example/admin/arkamilkproject/MainActivity.java
+++ app/src/main/java/com/example/admin/arkamilkproject/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "haii", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "haii", Toast.LENGTH_SHORT).show();
+
     }
 }
