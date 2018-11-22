3c6e14c05d419ae6bdba7c69a7529ed0a33b7764
Admin <mvallabhani@gmail.com>
2018-11-22 12:48:15 +0530

    mm

--- app/src/main/java/com/example/admin/arkamilkproject/Area.java
+++ app/src/main/java/com/example/admin/arkamilkproject/Area.java
@@ -4,20 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-    TextView signup;
-
+public class Area extends AppCompatActivity {
+    Button area;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        signup=(TextView)findViewById(R.id.Signup);
-        signup.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_area);
+        area=(Button)findViewById(R.id.area);
+        area.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,Registration.class);
+                Intent intent=new Intent(Area.this,Login.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/admin/arkamilkproject/Registration.java
+++ app/src/main/java/com/example/admin/arkamilkproject/Registration.java
@@ -6,18 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
-    TextView signup;
+public class Registration extends AppCompatActivity {
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        signup=(TextView)findViewById(R.id.Signup);
-        signup.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_registration);
+        login=(TextView)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,Registration.class);
+                Intent intent=new Intent(Registration.this,Login.class);
                 startActivity(intent);
             }
         });
