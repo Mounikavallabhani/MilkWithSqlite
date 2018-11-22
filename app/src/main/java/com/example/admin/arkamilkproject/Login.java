08ea74c5dd11b50542bde5c6be932ed8764c02cc
Admin <mvallabhani@gmail.com>
2018-11-22 13:27:05 +0530

    mm

--- app/src/main/java/com/example/admin/arkamilkproject/Login.java
+++ app/src/main/java/com/example/admin/arkamilkproject/Login.java
@@ -4,25 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class Area extends AppCompatActivity {
-    Button area;
+
+public class Login extends AppCompatActivity {
+    TextView signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_area);
-        area=(Button)findViewById(R.id.area);
-        area.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_login);
+        signup=(TextView)findViewById(R.id.Signup);
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Area.this, Login.class);
+                Intent intent=new Intent(Login.this,Registration.class);
                 startActivity(intent);
             }
         });
-        Toast.makeText(this, "haii", Toast.LENGTH_SHORT).show();
+
 
     }
 }
